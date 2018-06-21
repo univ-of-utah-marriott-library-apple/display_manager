8a629f1c3c25bc31ca899f952be8156dc8b0c3c5
Adam Davies <ahdavies6@gmail.com>
2018-06-21 09:46:26 -0600

    finally got temp.py to control rotate correctly! it's really ugly atm, but it'll do for now

--- temp.py
+++ temp.py
@@ -5,7 +5,7 @@
 
 import objc  # the actual bridge
 import Quartz  # Mac Display Services
-import sys
+import io  # for opening and reading .XML files
 
 
 def getIO():
@@ -17,18 +17,17 @@ def getIO():
         globals=globals()
     )
 
-    # todo: figure out wtf the bytestrings are actually supposed to be!!
     functions = [
         ("IOServiceRequestProbe", b"iI@o^I")
     ]
     # variables = [
     #     # The keys per angle of rotation
     #     ("kIOScaleRotate0", b""),
-    #     ("kIOScaleRotate90", b""),
+    #     ("kIOScaleRotate90", b"*"),
     #     ("kIOScaleRotate180", b""),
     #     ("kIOScaleRotate270", b""),
     #     # We'll see what this does soon enough
-    #     ("kIOFBSetTransform", b"*")
+    #     ("kIOFBSetTransform", b"I")
     # ]
 
     # todo: see if you can replace globals() with some other dict
@@ -36,38 +35,69 @@ def getIO():
     # objc.loadBundleVariables(iokit, globals(), variables)
 
 
-def getAngleOptions(angle):
-    rotateCodes = {0: 0, 90: 3, 180: 6, 270: 5}  # equivalent to kIOFBRotate{0, 90, 180, 270)
+def getRotateKey(angle):
+    rotateCodes = {0: 0, 90: 3, 180: 6, 270: 5}  # equivalent to kIOFBRotate{0, 90, 180, 270}
     setTransformCode = 1024  # equivalent to kIOFBSetTransform
     if angle % 90 == 0:
         # grab the right code, and move it to the right part of a 32-bit word
         rotateCode = rotateCodes[angle % 360] << 16
-        # or the two together
-        return setTransformCode | rotateCode
+        # "or" the two together
+        return int(setTransformCode | rotateCode)
     else:  # inappropriate angle
         return None
 
 
 def rotate(display, degrees):
-    options = getAngleOptions(degrees)
+    options = getRotateKey(degrees)
     if options:  # the angle was an appropriate number of degrees
         service = Quartz.CGDisplayIOServicePort(display)
-
-        # todo: delete these lines:
-        # options = int(hex(1024 | options), 16)
-        # options = 197632
-
         IOServiceRequestProbe(service, options, None)  # actually sets the rotation
 
 
-def main():
-    getIO()
-    rotate(Quartz.CGMainDisplayID(), int(sys.argv[1]))
+# todo: rename this
+def tempRename(display):
 
-    # print(kIOScaleRotate0 == getAngleOptions(0))
-    # print(kIOScaleRotate90 == getAngleOptions(90))
-    # print(kIOScaleRotate180 == getAngleOptions(180))
-    # print(kIOScaleRotate270 == getAngleOptions(270))
+    xmlString = """
+    <?xml version='1.0'?>
+    <!DOCTYPE signatures SYSTEM "file://localhost/System/Library/DTDs/BridgeSupport.dtd">
+    <signatures version='1.0'>
+    <function name='IOServiceRequestProbe'>
+    <arg type='I'/>
+    <arg type='I'/>
+    <retval type='i'/>
+    </function>
+    </signatures>
+    """
+
+    # with io.open("../PyRotate/iokit.metadata", "r", encoding="utf-8") as cont:
+    #     IOKIT_XML = cont.read()
+    # objc.parseBridgeSupport(IOKIT_XML, globals(), objc.pathForFramework("/System/Library/Frameworks/IOKit.framework"))
+    objc.parseBridgeSupport(xmlString, globals(), objc.pathForFramework("/System/Library/Frameworks/IOKit.framework"))
+
+    # with io.open("../PyRotate/iodisp.metadata", "r", encoding="utf-8") as cont:
+    #     IODISP_XML = cont.read()
+    # objc.parseBridgeSupport(IODISP_XML, globals(), objc.pathForFramework("/System/Library/Frameworks/IOKit.framework"))
+
+    service = Quartz.CGDisplayIOServicePort(display)
+    # options = (0x00000400 | (kIOScaleRotate90) << 16)
+    options = (0x00000400 | (48) << 16)
+    IOServiceRequestProbe(service, options)
+
+    from time import sleep
+    sleep(2)
+
+    # options = (0x00000400 | (kIOScaleRotate0) << 16)
+    # options = (0x00000400 | (0) << 16)
+    options = 0x00000400
+    IOServiceRequestProbe(service, options)
+
+    # (0, 0), (90, 48), (180, 96), (80, 80)
+
+
+def main():
+    # getIO()
+    # rotate(Quartz.CGMainDisplayID(), int(sys.argv[1]))
+    tempRename(Quartz.CGMainDisplayID())
 
 
 if __name__ == "__main__":
