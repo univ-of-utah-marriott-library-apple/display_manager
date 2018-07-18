7ea5ff5d1c2a9a6443da04bdbb4714a1bb12ff6f
Adam Davies <ahdavies6@gmail.com>
2018-07-17 20:41:00 -0600

    brought back old GUI app; now have build_py2app and build_pyinstaller. both broken on 10.11

---
+++ installer/app/setIcon.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import Cocoa
+import sys
+
+Cocoa.NSWorkspace.sharedWorkspace().setIcon_forFile_options_(
+    Cocoa.NSImage.alloc().initWithContentsOfFile_(sys.argv[1].decode('utf-8')),
+    sys.argv[2].decode('utf-8'),
+    0
+)
