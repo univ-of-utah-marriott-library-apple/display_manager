7b774e059c90c2a079895c649e7dc9fce7c4cc81
Adam Davies <ahdavies6@gmail.com>
2018-08-14 14:33:27 -0600

    added copyright into the three main files

--- display_manager.py
+++ display_manager.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python
 
+########################################################################
+# Copyright (c) 2018 University of Utah Student Computing Labs.        #
+# All Rights Reserved.                                                 #
+#                                                                      #
+# Permission to use, copy, modify, and distribute this software and    #
+# its documentation for any purpose and without fee is hereby granted, #
+# provided that the above copyright notice appears in all copies and   #
+# that both that copyright notice and this permission notice appear    #
+# in supporting documentation, and that the name of The University     #
+# of Utah not be used in advertising or publicity pertaining to        #
+# distribution of the software without specific, written prior         #
+# permission. This software is supplied as is without expressed or     #
+# implied warranties of any kind.                                      #
+########################################################################
+
 # Display Manager, version 1.0.0
 # Command-Line Interface
 
--- gui.py
+++ gui.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python
 
-# A GUI that allows users to interface with Display Manager
+########################################################################
+# Copyright (c) 2018 University of Utah Student Computing Labs.        #
+# All Rights Reserved.                                                 #
+#                                                                      #
+# Permission to use, copy, modify, and distribute this software and    #
+# its documentation for any purpose and without fee is hereby granted, #
+# provided that the above copyright notice appears in all copies and   #
+# that both that copyright notice and this permission notice appear    #
+# in supporting documentation, and that the name of The University     #
+# of Utah not be used in advertising or publicity pertaining to        #
+# distribution of the software without specific, written prior         #
+# permission. This software is supplied as is without expressed or     #
+# implied warranties of any kind.                                      #
+########################################################################
+
+# Display Manager, version 1.0.0
+# Graphical User Interface
 
 import os
 import Tkinter as tk
